24e31e52767fde9b48ffb17fffa1f05853ae594a
William Le <email@wle8300.com>
2019-12-05 11:03:39 -0800

    add TypeScript definitions

---
+++ src/index.d.ts
@@ -0,0 +1,16 @@
+declare module 'uifx' {
+  
+  type config = {
+    volume?: number;
+    throttleMs?: number;
+  };
+
+  export default class UIFx {
+    
+    constructor(file: string, config?: config) {
+    }
+
+    play: (volume: number) => UIFx;
+    setVolume: (volume: number) => UIFx;
+  }
+}
